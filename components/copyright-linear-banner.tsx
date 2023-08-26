069e8ea1a71031a3244507ac9e693094dcb52bbf
Bimal Ray <72511459+zenn99-arch@users.noreply.github.com>
2023-08-26 09:37:25 +0530

    chanegd

--- components/copyright-linear-banner.tsx
+++ components/copyright-linear-banner.tsx
@@ -27,10 +27,7 @@ export const CopyrightLinearBanner = () => {
     <div className="fixed right-[4rem] bottom-[4rem] flex w-[65rem] max-w-[95%] items-start rounded-md border border-transparent-white p-8 text-[2rem] backdrop-blur-[12px]">
       <div className="space-y-3">
         <p>
-          <strong>⚠️ NOTE:</strong> I am not affiliated with Linear in any way.
-          All images, the name of Linear and their logo are copyrighted by
-          Linear. This website is only intended for educational purposes and as
-          a homage to Linear's great UI work 🙏
+          <strong></strong> enjoy the webclone version of the website 
         </p>
         <p>
           <Button href="https://linear.app" size="large" target="_blank">
